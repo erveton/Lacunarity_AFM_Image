4526cf3877e737e509c58b1377c39ffd66643e68
Erveton Pinheiro Pinto <44680050+erveton@users.noreply.github.com>
2020-10-14 14:19:37 -0300

    Add files via upload

---
+++ Lacunaridade Filmes Finos_N=3.R
@@ -0,0 +1,84 @@
+#Program to calculate the lacunarity of Thin Films
+
+#Step 1: create a variable that contains the height matrix from the specific directory.
+
+image1 <- read.table (file = "sample.txt")
+
+#Step 2: indicate the Otsu threshold.
+
+threshold <- "k*" #Otsu threshold
+
+#Step 3: transform the height values into binary data.
+
+z<-ifelse (image1[3] >= threshold, "1","0")
+
+#Step 4: Create a binary matrix with the variable z.
+
+mat<-matrix (z, nrow = 256, ncol = 256, byrow = TRUE)
+
+#Step 5: Choose the box size.
+
+N=3
+
+#Step 6: cover the entire binary matrix with boxes of size N and count the number of lacunar pixels in each box.
+
+vnum0 = c(NULL)
+for (n in c(1:254)) {
+  
+  i<-c(1+(n-1),2+(n-1),3+(n-1))
+  
+  for (k in c(2:255)){
+    
+    j<-c(k-1,k,k+1)
+    
+    M<-matrix(mat[i,j],nrow=N, ncol=N)
+    
+    soma<-sum(M==0)
+    vnum0 = c(vnum0,soma)
+    
+  }}
+
+#Step 7: count the number of boxes with s (s = 0,1,2,3,4, ..., NxN) lacunar pixels.
+
+ns<-table(vnum0)
+
+print(ns)
+
+#Step 8: calculate the probability of occupying of the boxes.
+
+Pns<-ns/(256-N+1)^(2)
+
+print(Pns)
+
+#Step 9: calculate the first and second moments of the s.
+
+s<-c(0:9)
+
+M1<-(s)*Pns
+
+print(M1)
+
+M2<-(s^2)*Pns
+
+print(M2)
+
+#Step 10: calculate the lacunarity.
+
+L<-(sum(M2))/((sum(M1))^2)
+
+#calculate the ln of N
+
+logr<-log(N)
+
+#calculate the ln of L
+
+logL<-log(L)
+
+# Sort results for visualization
+
+result = data.frame(N,L,logN,logL)
+colnames(result) = c("Size box", "Lacunarity", "logN", "logL")
+
+# View result
+result
+
---
+++ Lacunaridade Filmes Finos_N=5.R
@@ -0,0 +1,82 @@
+#Programa para  calcular a lacunaridade de Filmes Finos
+
+#Passo 1: criar uma variável que contenha a matriz de alturas a partir do diretório específico.
+
+image1<-read.table (file = "sample.txt")
+
+#Passo 2: incicar o limiar.
+
+limiar<-"k*" #limiar Otsu
+
+#Passo 3: Transformar os valores de alturas em dados binários
+
+z<-ifelse (image1[3] >= limiar, "1","0")
+
+#Passo 4: Criar uma matriz binária com a variável z.
+
+mat<-matrix (z, nrow = 256, ncol = 256, byrow = TRUE)
+
+#Passo 5: Escolher o tamanho da caixa.
+
+N=5
+
+#Passo 6: cobrir toda a matriz binária com as caixas de tamanho N e Contar o número de pixels lacunares em cada caixa.
+
+vnum0 = c(NULL)
+for (n in c(1:252)) {
+  
+  i<-c(1+(n-1),2+(n-1),3+(n-1),4+(n-1),5+(n-1))
+  
+  for (k in c(2:253)){
+    
+    j<-c(k-1,k,k+1,k+2,k+3)
+    
+    M<-matrix(mat[i,j],nrow=N, ncol=N)
+    
+    soma<-sum(M==0)
+    vnum0 = c(vnum0,soma)
+    
+  }}
+
+#Passo 7: contar o número de caixas com s (s=0,1,2,3,4,...,NxN) pixels lacunares.
+
+ns<-table(vnum0)
+
+print(ns)
+
+#Passo 8: calcular a probabilidade de ocupação das caixas.
+
+Pns<-ns/(256-N+1)^(2)
+
+print(Pns)
+
+#Passo 9: calcular o primeiro e o segundo momento do s.
+
+s<-c(0:25)
+
+M1<-(s)*Pns
+
+print(M1)
+
+M2<-(s^2)*Pns
+
+print(M2)
+
+#Passo 10: calcular a lacunaridade.
+
+L<-(sum(M2))/((sum(M1))^2)
+
+#calcular o ln de N
+
+logN<-log(N)
+
+#calcular o ln de L
+
+logL<-log(L)
+
+# Arrumar resultados para visualizacao
+result = data.frame(N,L,logN,logL)
+colnames(result) = c("Size box", "Lacunarity", "logN", "logL")
+
+# Ver resultado
+result
